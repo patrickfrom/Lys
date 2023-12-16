423ce371246d5a53cee678d64be1256adb0ea655
Patrick From <Patrickfromgnomedk@gmail.com>
2023-12-16 22:10:07 +0100

    Vertex!

---
+++ Lys/Vertex.cs
@@ -0,0 +1,10 @@
+﻿using OpenTK.Mathematics;
+
+namespace Lys;
+
+public struct Vertex(Vector3 position, Vector3 normals, Vector2 texCoords)
+{
+    public Vector3 Position = position;
+    public Vector3 Normals = normals;
+    public Vector2 TexCoords = texCoords;
+}
--- Lys/Window.cs
+++ Lys/Window.cs
@@ -15,33 +15,84 @@ public class Window(int width, int height, string title) : GameWindow(GameWindow
         Title = title
     })
 {
-
     private ImGuiController _controller;
-    
+
+    private readonly Vertex[] _vertices =
+    {
+        new(new Vector3(-0.5f, -0.5f, -0.5f), new Vector3(0.0f, 0.0f, -1.0f), new Vector2(0.0f, 0.0f)),
+        new(new Vector3(0.5f, -0.5f, -0.5f), new Vector3(0.0f, 0.0f, -1.0f), new Vector2(1.0f, 0.0f)),
+        new(new Vector3(0.5f, 0.5f, -0.5f), new Vector3(0.0f, 0.0f, -1.0f), new Vector2(1.0f, 1.0f)),
+        new(new Vector3(-0.5f, 0.5f, -0.5f), new Vector3(0.0f, 0.0f, -1.0f), new Vector2(0.0f, 1.0f)),
+
+        new(new Vector3(-0.5f, -0.5f, 0.5f), new Vector3(0.0f, 0.0f, 1.0f), new Vector2(0.0f, 0.0f)),
+        new(new Vector3(0.5f, -0.5f, 0.5f), new Vector3(0.0f, 0.0f, 1.0f), new Vector2(1.0f, 0.0f)),
+        new(new Vector3(0.5f, 0.5f, 0.5f), new Vector3(0.0f, 0.0f, 1.0f), new Vector2(1.0f, 1.0f)),
+        new(new Vector3(-0.5f, 0.5f, 0.5f), new Vector3(0.0f, 0.0f, 1.0f), new Vector2(0.0f, 1.0f)),
+
+        new(new Vector3(-0.5f, 0.5f, -0.5f), new Vector3(-1.0f, 0.0f, 0.0f), new Vector2(0.0f, 0.0f)),
+        new(new Vector3(-0.5f, -0.5f, -0.5f), new Vector3(-1.0f, 0.0f, 0.0f), new Vector2(1.0f, 0.0f)),
+        new(new Vector3(-0.5f, -0.5f, 0.5f), new Vector3(-1.0f, 0.0f, 0.0f), new Vector2(1.0f, 1.0f)),
+        new(new Vector3(-0.5f, 0.5f, 0.5f), new Vector3(-1.0f, 0.0f, 0.0f), new Vector2(0.0f, 1.0f)),
+
+        new(new Vector3(0.5f, 0.5f, 0.5f), new Vector3(1.0f, 0.0f, 0.0f), new Vector2(0.0f, 0.0f)),
+        new(new Vector3(0.5f, 0.5f, -0.5f), new Vector3(1.0f, 0.0f, 0.0f), new Vector2(1.0f, 0.0f)),
+        new(new Vector3(0.5f, -0.5f, -0.5f), new Vector3(1.0f, 0.0f, 0.0f), new Vector2(1.0f, 1.0f)),
+        new(new Vector3(0.5f, -0.5f, 0.5f), new Vector3(1.0f, 0.0f, 0.0f), new Vector2(0.0f, 1.0f)),
+
+        new(new Vector3(-0.5f, -0.5f, -0.5f), new Vector3(0.0f, -1.0f, 0.0f), new Vector2(0.0f, 0.0f)),
+        new(new Vector3(0.5f, -0.5f, -0.5f), new Vector3(0.0f, -1.0f, 0.0f), new Vector2(1.0f, 0.0f)),
+        new(new Vector3(0.5f, -0.5f, 0.5f), new Vector3(0.0f, -1.0f, 0.0f), new Vector2(1.0f, 1.0f)),
+        new(new Vector3(-0.5f, -0.5f, 0.5f), new Vector3(0.0f, -1.0f, 0.0f), new Vector2(0.0f, 1.0f)),
+
+        new(new Vector3(0.5f, 0.5f, -0.5f), new Vector3(0.0f, 1.0f, 0.0f), new Vector2(0.0f, 0.0f)),
+        new(new Vector3(0.5f, 0.5f, 0.5f), new Vector3(0.0f, 1.0f, 0.0f), new Vector2(1.0f, 0.0f)),
+        new(new Vector3(-0.5f, 0.5f, 0.5f), new Vector3(0.0f, 1.0f, 0.0f), new Vector2(1.0f, 1.0f)),
+        new(new Vector3(-0.5f, 0.5f, -0.5f), new Vector3(0.0f, 1.0f, 0.0f), new Vector2(0.0f, 1.0f)),
+    };
+
+    private readonly int[] _indices =
+    {
+        0, 3, 1,
+        3, 2, 1,
+
+        4, 5, 7,
+        7, 5, 6,
+
+        8, 9, 11,
+        11, 9, 10,
+
+        12, 15, 13,
+        15, 14, 13,
+
+        16, 17, 19,
+        19, 17, 18,
+
+        20, 23, 21,
+        23, 22, 21,
+    };
+
     protected override void OnLoad()
     {
         GlDebugger.Init();
         GL.ClearColor(Color.Navy);
-        
+
         _controller = new ImGuiController(ClientSize.X, ClientSize.Y);
     }
 
     protected override void OnRenderFrame(FrameEventArgs e)
     {
         GL.Clear(ClearBufferMask.ColorBufferBit);
-        
+
         _controller.Update(this, (float)e.Time);
 
         ImGui.DockSpaceOverViewport();
-        ImGui.ShowDemoWindow();
-        
+
         _controller.Render();
         SwapBuffers();
     }
 
     protected override void OnUpdateFrame(FrameEventArgs e)
     {
-
     }
 
     protected override void OnResize(ResizeEventArgs e)
@@ -49,7 +100,7 @@ public class Window(int width, int height, string title) : GameWindow(GameWindow
         GL.Viewport(0, 0, ClientSize.X, ClientSize.Y);
         _controller.WindowResized(ClientSize.X, ClientSize.Y);
     }
-    
+
     protected override void OnTextInput(TextInputEventArgs e)
     {
         base.OnTextInput(e);
